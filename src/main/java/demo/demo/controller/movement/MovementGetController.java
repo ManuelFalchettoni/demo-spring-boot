7b84baac400df13ae206a6a0fe42c024876c07dc
ManuelFalchettoni <manufalchettoni@gmail.com>
2026-04-16 10:34:12 -0300

    fix: use convertor on GET controller

--- src/main/java/demo/demo/controller/movement/MovementGetController.java
+++ src/main/java/demo/demo/controller/movement/MovementGetController.java
@@ -1,6 +1,7 @@
 package demo.demo.controller.movement;
 
 import demo.demo.dto.response.movement.MovementResponse;
+import demo.demo.mapper.movement.MovementMapper;
 import demo.demo.model.movement.Movement;
 import demo.demo.service.movement.MovementFinderService;
 import org.springframework.http.ResponseEntity;
@@ -14,21 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class MovementGetController {
     //Instanciar
     private final MovementFinderService movementFinderService;
+    private final MovementMapper movementMapper;
     //Inicializar con constructor
-    public MovementGetController(MovementFinderService movementFinderService){
+    public MovementGetController(MovementFinderService movementFinderService, MovementMapper movementMapper){
         this.movementFinderService = movementFinderService;
+        this.movementMapper = movementMapper;
     }
 
 
     @GetMapping("/{id}") //Se usara todo despues de la barra como paramatero para el ID
     public ResponseEntity<MovementResponse> find(@PathVariable Long id){ //Devolvemos un ResponseEntity
         Movement movement = movementFinderService.find(id); //Guardamos la entidad que nos devulve el service con ese id
-        MovementResponse movementResponse = new MovementResponse(
-                movement.getId(),
-                movement.getName(),
-                movement.getDescription()
-        );//Creamos una nueva entidad guardando los atributos que necesitamos, e ignorando los que no
-        return ResponseEntity.ok(movementResponse); //Devolvemos el ResponseEntity con OK
+        MovementResponse response = movementMapper.convertor(movement);//Creamos una nueva entidad guardando los atributos que necesitamos, e ignorando los que no
+        return ResponseEntity.ok(response); //Devolvemos el ResponseEntity con OK
     }
 
 }
