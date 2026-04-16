fadd58ff74ef691be316bb6f1757da92d5cfa114
ManuelFalchettoni <manufalchettoni@gmail.com>
2026-04-16 12:20:20 -0300

    refactor: Mapper do the conversion toEntity and to Response in Post Controller

--- src/main/java/demo/demo/controller/movement/MovementPostController.java
+++ src/main/java/demo/demo/controller/movement/MovementPostController.java
@@ -1,6 +1,8 @@
 package demo.demo.controller.movement;
 
 import demo.demo.dto.request.movement.MovementRequest;
+import demo.demo.dto.response.movement.MovementResponse;
+import demo.demo.mapper.movement.MovementMapper;
 import demo.demo.model.movement.Movement;
 import demo.demo.service.movement.MovementCreatorService;
 import org.springframework.http.HttpStatus;
@@ -16,17 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class MovementPostController {
     //Instanciar
     private final MovementCreatorService movementCreatorService;
+    private final MovementMapper movementMapper;
 
     //Inicializar con cosntructor
-    public MovementPostController(MovementCreatorService movementCreatorService){
+    public MovementPostController(MovementCreatorService movementCreatorService, MovementMapper movementMapper){
         this.movementCreatorService = movementCreatorService;
+        this.movementMapper = movementMapper;
     }
 
     @PostMapping //Mapping con el tipo de request que estamos usando
-    public ResponseEntity<Movement> create(@RequestBody MovementRequest movementRequest){
-        Movement movement= new Movement(); //   Nueva entidad
-        movement.setName(movementRequest.getName()); // Set name
-        movement.setDescription(movementRequest.getDescription()); //Set description
-        return ResponseEntity.status(HttpStatus.CREATED).body(this.movementCreatorService.create(movement)); //Devolvemos Response con status, a traves del body, utilizando el service de creator
+    public ResponseEntity<MovementResponse> create(@RequestBody MovementRequest movementRequest){
+        Movement movement = movementMapper.toEntity(movementRequest);
+        Movement savedMovement = movementCreatorService.create(movement);
+        MovementResponse response = movementMapper.convertor(savedMovement);//Set description
+        return ResponseEntity.status(HttpStatus.CREATED).body(response); //Devolvemos Response con status, a traves del body, utilizando el service de creator
     }
 }
--- src/main/java/demo/demo/mapper/movement/MovementMapper.java
+++ src/main/java/demo/demo/mapper/movement/MovementMapper.java
@@ -1,5 +1,6 @@
 package demo.demo.mapper.movement;
 
+import demo.demo.dto.request.movement.MovementRequest;
 import demo.demo.dto.response.movement.MovementResponse;
 import demo.demo.model.movement.Movement;
 import org.springframework.stereotype.Component;
@@ -10,5 +11,12 @@ public class MovementMapper {
 
     public MovementResponse convertor(Movement movement){
         return new MovementResponse (movement.getId(), movement.getName(), movement.getDescription());
+    }
+
+    public Movement toEntity(MovementRequest movementRequest){
+        Movement movement = new Movement();
+        movement.setName(movementRequest.getName());
+        movement.setDescription(movementRequest.getDescription());
+        return movement;
     }
 }
